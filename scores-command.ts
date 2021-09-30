c9154102f79b7b045c948ddfa977eb5d5aa40d6c
Zlatibor Veljkovic <zveljkovic@hotmail.com>
2021-09-30 16:33:10 +0200

    asdf

--- scores-command.ts
+++ scores-command.ts
@@ -1,9 +1,15 @@
 import {SlackCommandMiddlewareArgs} from '@slack/bolt/dist/types';
+import {getCustomRepository} from 'typeorm';
+import {Team} from './entities/Team';
+import {TeamRepository} from './repositories/TeamRepository';
 
 export const scoresCommand = async ({ command, ack, respond }: SlackCommandMiddlewareArgs) => {
     // Acknowledge command request
     await ack();
 
-
-    await respond(`${command.text}`);
+    const teamRepository = getCustomRepository(TeamRepository);
+    const teams = await teamRepository.getAll();
+    teams.sort((a: Team, b: Team) => b.score - a.score);
+    const response = teams.map(t => `${t.teamName} has ${t.score} points`).join('\n');
+    await respond(response);
 }
