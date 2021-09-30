34548c2230021da76f6ba9b1c2231babaf54e277
Zlatibor Veljkovic <zveljkovic@hotmail.com>
2021-09-30 12:16:04 +0200

    df

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const app = new App({
     appToken: process.env.SLACK_APP_TOKEN
 });
 
+console.log({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN
+});
 
 (async () => {
     // Start your app
@@ -17,5 +23,6 @@ const app = new App({
     app.message('hello', async ({message, say}) => {
         await say(`Hello there <@${message.user}`)
     })
+
     console.log('⚡️ Bolt app is running!');
 })();
